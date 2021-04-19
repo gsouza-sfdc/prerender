a5d8fbf5b1b5216bed9a4c30ba58fce17dce3ff5
Gustavo Souza <gsouza@salesforce.com>
2021-04-19 16:56:55 -0700

    Inject ShadyDOM JS, omit prerender headers for localhost development, add env variables

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ var prerender = require('./lib');
 
 var server = prerender();
 
-server.use(prerender.sendPrerenderHeader());
+if (!server.options.omitPrerenderHeader) {
+  // Doing this for localhost requests causes CORS errors.
+  // server.use(prerender.sendPrerenderHeader());
+}
+
 // server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
